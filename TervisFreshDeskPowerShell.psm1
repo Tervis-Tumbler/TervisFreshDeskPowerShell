ddeeb1b0a1aef10c4138664d1a6ada0f96e5eb49
ChrisMagnuson <hourglasssand@gmail.com>
2018-06-15 09:47:16 -0400

    Update to better layout

--- TervisFreshDeskPowerShell.psm1
+++ TervisFreshDeskPowerShell.psm1
@@ -287,93 +287,90 @@ function New-TervisWarrantyFormDashboard {
             }
         }
         
-        New-UDRow {
-            New-UDColumn {
-                New-UDInput -Title "New Warranty Child" -Id "NewWarrantyChildInput" -Endpoint {
-                    param (
-                        $DesignName,
-                        [ValidateSet(
-                            "10oz (5 1/2)",
-                            "12oz (4 1/4)",
-                            "wavy (5 1/2)",
-                            "wine glass (8 1/2)",
-                            "My First Tervis Sippy Cup (5 1/5)",
-                            "16oz (6)",
-                            "mug (5)",
-                            "stemless wine glass (4 4/5)",
-                            "24oz (7 7/8)",
-                            "water bottle (10.4)",
-                            "8oz (4)",
-                            "goblet (7 7/8)",
-                            "collectible (2 3/4)",
-                            "tall (6 1/4)",
-                            "stout (3 1/2)",
-                            "20oz Stainless Steel (6 3/4)",
-                            "30oz Stainless Steel (8)"
-                        )]
-                        [String]$Size,
+        New-UDLayout -Columns 2 -Content {
+            New-UDInput -Title "New Warranty Child" -Id "NewWarrantyChildInput" -Endpoint {
+                param (
+                    $DesignName,
+                    [ValidateSet(
+                        "10oz (5 1/2)",
+                        "12oz (4 1/4)",
+                        "wavy (5 1/2)",
+                        "wine glass (8 1/2)",
+                        "My First Tervis Sippy Cup (5 1/5)",
+                        "16oz (6)",
+                        "mug (5)",
+                        "stemless wine glass (4 4/5)",
+                        "24oz (7 7/8)",
+                        "water bottle (10.4)",
+                        "8oz (4)",
+                        "goblet (7 7/8)",
+                        "collectible (2 3/4)",
+                        "tall (6 1/4)",
+                        "stout (3 1/2)",
+                        "20oz Stainless Steel (6 3/4)",
+                        "30oz Stainless Steel (8)"
+                    )]
+                    [String]$Size,
+            
+                    [ValidateSet("1","2","3","4","5","6","7","8","9","10")][String]$Quantity,
+                    [ValidateSet(
+                        "Before 2004","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","NA"
+                    )][String]$ManufactureYear,
+            
+                    [ValidateSet("cracked","decoration fail","film","heat distortion","stainless defect","seal failure")][String]$ReturnReason
+                )
+                $CachedData = Get-Item Cache:$GUID
+                $WarrantyRequestLine = New-WarrantyRequestLine @PSBoundParameters
+                $WarrantyChildTicket = $WarrantyRequestLine | New-WarrantyChildTicket -WarrantyRequest $CachedData.WarrantyRequest -WarrantyParentTicket $CachedData.WarrantyRequest.Ticket
                 
-                        [ValidateSet("1","2","3","4","5","6","7","8","9","10")][String]$Quantity,
-                        [ValidateSet(
-                            "Before 2004","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","NA"
-                        )][String]$ManufactureYear,
-                
-                        [ValidateSet("cracked","decoration fail","film","heat distortion","stainless defect","seal failure")][String]$ReturnReason
+                $CachedData.WarrantyRequestLine.Add(
+                    $(
+                        $WarrantyRequestLine |
+                        Add-Member -MemberType NoteProperty -Name Ticket -Value $WarrantyChildTicket -PassThru
                     )
-                    $CachedData = Get-Item Cache:$GUID
-                    $WarrantyRequestLine = New-WarrantyRequestLine @PSBoundParameters
-                    $WarrantyChildTicket = $WarrantyRequestLine | New-WarrantyChildTicket -WarrantyRequest $CachedData.WarrantyRequest -WarrantyParentTicket $CachedData.WarrantyRequest.Ticket
-                    
-                    $CachedData.WarrantyRequestLine.Add(
-                        $(
-                            $WarrantyRequestLine |
-                            Add-Member -MemberType NoteProperty -Name Ticket -Value $WarrantyChildTicket -PassThru
-                        )
-                    )
+                )
 
-                    #New-UDInputAction -ClearInput -Toast "Warranty Line Created" #Given we are redirecting the whole page below I don't know that we need this
-                    Add-UDElement -ParentId "RedirectParent" -Content {
-                        New-UDHtml -Markup @"
-                        <meta http-equiv="refresh" content="0; URL='/WarrantyChild/$GUID'" />
+                #New-UDInputAction -ClearInput -Toast "Warranty Line Created" #Given we are redirecting the whole page below I don't know that we need this
+                Add-UDElement -ParentId "RedirectParent" -Content {
+                    New-UDHtml -Markup @"
+                    <meta http-equiv="refresh" content="0; URL='/WarrantyChild/$GUID'" />
 "@
-                    }
                 }
             }
-            new-UDColumn {
-                New-UDTable -Title "Warranty Child" -Id "WarrantyChildTable" -Headers DesignName, Size, Quantity, ManufactureYear, ReturnReason, Action -Endpoint {
-                    $CachedData = Get-Item Cache:$GUID
-                    $CachedData.WarrantyRequestLine |
-                    ForEach-Object {
-                        $_ | Select-Object -Property @{
-                            Name = "ID"
-                            Expression = {$CachedData.WarrantyParentTicket.ID}
-                        }, *, 
-                        @{
-                            Name = "Remove"
-                            Expression = {
-                                New-UDElement -Tag "a" -Attributes @{
-                                    className = "btn"
-                                    onClick = {
-                                        $CachedData = Get-Item Cache:$GUID
-                                        $_.Ticket | Remove-FreshDeskTicket
-                                        
-                                        $CachedData.WarrantyRequestLine.Remove($_)
 
-                                        Add-UDElement -ParentId "RedirectParent" -Content {
-                                            New-UDHtml -Markup @"
-                                                <meta http-equiv="refresh" content="0; URL='/WarrantyChild/$GUID'" />
+            New-UDTable -Title "Warranty Child" -Id "WarrantyChildTable" -Headers DesignName, Size, Quantity, ManufactureYear, ReturnReason, Action -Endpoint {
+                $CachedData = Get-Item Cache:$GUID
+                $CachedData.WarrantyRequestLine |
+                ForEach-Object {
+                    $_ | Select-Object -Property @{
+                        Name = "ID"
+                        Expression = {$CachedData.WarrantyParentTicket.ID}
+                    }, *, 
+                    @{
+                        Name = "Remove"
+                        Expression = {
+                            New-UDElement -Tag "a" -Attributes @{
+                                className = "btn"
+                                onClick = {
+                                    $CachedData = Get-Item Cache:$GUID
+                                    $_.Ticket | Remove-FreshDeskTicket
+                                    
+                                    $CachedData.WarrantyRequestLine.Remove($_)
+
+                                    Add-UDElement -ParentId "RedirectParent" -Content {
+                                        New-UDHtml -Markup @"
+                                            <meta http-equiv="refresh" content="0; URL='/WarrantyChild/$GUID'" />
 "@
-                                        }
                                     }
-                                } -Content {
-                                    "Remove" 
-                                } 
-                            }
+                                }
+                            } -Content {
+                                "Remove" 
+                            } 
                         }
-                    } |
-                    Out-UDTableData -Property DesignName, Size, Quantity, ManufactureYear, ReturnReason, Remove
-                } #-AutoRefresh -RefreshInterval 2  
-            }
+                    }
+                } |
+                Out-UDTableData -Property DesignName, Size, Quantity, ManufactureYear, ReturnReason, Remove
+            } #-AutoRefresh -RefreshInterval 2  
         }
     }
 	
