f6a11cca7a18d78d81f5cfe5fd32cea581bde65a
ChrisMagnuson <hourglasssand@gmail.com>
2018-06-20 16:05:25 -0400

    Quick code to pull values from html drop down values

---
+++ MethodOfPullingOptionValuesToBeUsed.ps1
@@ -0,0 +1,469 @@
+(@"
+<ul aria-controls="ember-power-select-trigger-ember2472" role="listbox" id="ember-power-select-options-ember2472" class="ember-power-select-options ember-view"><!---->    <li aria-selected="false" aria-current="true" data-option-index="0" role="option" class="ember-power-select-option">
+                          <span id="ember3538" class="ember-view">  None
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="1" role="option" class="ember-power-select-option">
+                          <span id="ember3539" class="ember-view">  AL
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="2" role="option" class="ember-power-select-option">
+                          <span id="ember3540" class="ember-view">  AK
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="3" role="option" class="ember-power-select-option">
+                          <span id="ember3541" class="ember-view">  AZ
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="4" role="option" class="ember-power-select-option">
+                          <span id="ember3542" class="ember-view">  AR
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="5" role="option" class="ember-power-select-option">
+                          <span id="ember3543" class="ember-view">  CA
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="6" role="option" class="ember-power-select-option">
+                          <span id="ember3544" class="ember-view">  CO
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="7" role="option" class="ember-power-select-option">
+                          <span id="ember3545" class="ember-view">  CT
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="8" role="option" class="ember-power-select-option">
+                          <span id="ember3546" class="ember-view">  DE
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="9" role="option" class="ember-power-select-option">
+                          <span id="ember3547" class="ember-view">  FL
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="10" role="option" class="ember-power-select-option">
+                          <span id="ember3548" class="ember-view">  GA
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="11" role="option" class="ember-power-select-option">
+                          <span id="ember3549" class="ember-view">  HI
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="12" role="option" class="ember-power-select-option">
+                          <span id="ember3550" class="ember-view">  ID
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="13" role="option" class="ember-power-select-option">
+                          <span id="ember3551" class="ember-view">  IL
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="14" role="option" class="ember-power-select-option">
+                          <span id="ember3552" class="ember-view">  IN
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="15" role="option" class="ember-power-select-option">
+                          <span id="ember3553" class="ember-view">  IA
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="16" role="option" class="ember-power-select-option">
+                          <span id="ember3554" class="ember-view">  KS
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="17" role="option" class="ember-power-select-option">
+                          <span id="ember3555" class="ember-view">  KY
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="18" role="option" class="ember-power-select-option">
+                          <span id="ember3556" class="ember-view">  LA
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="19" role="option" class="ember-power-select-option">
+                          <span id="ember3557" class="ember-view">  ME
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="20" role="option" class="ember-power-select-option">
+                          <span id="ember3558" class="ember-view">  MD
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="21" role="option" class="ember-power-select-option">
+                          <span id="ember3559" class="ember-view">  MA
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="22" role="option" class="ember-power-select-option">
+                          <span id="ember3560" class="ember-view">  MI
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="23" role="option" class="ember-power-select-option">
+                          <span id="ember3561" class="ember-view">  MN
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="24" role="option" class="ember-power-select-option">
+                          <span id="ember3562" class="ember-view">  MS
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="25" role="option" class="ember-power-select-option">
+                          <span id="ember3563" class="ember-view">  MO
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="26" role="option" class="ember-power-select-option">
+                          <span id="ember3564" class="ember-view">  MT
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="27" role="option" class="ember-power-select-option">
+                          <span id="ember3565" class="ember-view">  NE
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="28" role="option" class="ember-power-select-option">
+                          <span id="ember3566" class="ember-view">  NV
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="29" role="option" class="ember-power-select-option">
+                          <span id="ember3567" class="ember-view">  NH
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="30" role="option" class="ember-power-select-option">
+                          <span id="ember3568" class="ember-view">  NJ
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="31" role="option" class="ember-power-select-option">
+                          <span id="ember3569" class="ember-view">  NM
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="32" role="option" class="ember-power-select-option">
+                          <span id="ember3570" class="ember-view">  NY
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="33" role="option" class="ember-power-select-option">
+                          <span id="ember3571" class="ember-view">  NC
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="34" role="option" class="ember-power-select-option">
+                          <span id="ember3572" class="ember-view">  ND
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="35" role="option" class="ember-power-select-option">
+                          <span id="ember3573" class="ember-view">  OH
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="36" role="option" class="ember-power-select-option">
+                          <span id="ember3574" class="ember-view">  OK
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="37" role="option" class="ember-power-select-option">
+                          <span id="ember3575" class="ember-view">  OR
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="38" role="option" class="ember-power-select-option">
+                          <span id="ember3576" class="ember-view">  PA
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="39" role="option" class="ember-power-select-option">
+                          <span id="ember3577" class="ember-view">  RI
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="40" role="option" class="ember-power-select-option">
+                          <span id="ember3578" class="ember-view">  SC
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="41" role="option" class="ember-power-select-option">
+                          <span id="ember3579" class="ember-view">  SD
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="42" role="option" class="ember-power-select-option">
+                          <span id="ember3580" class="ember-view">  TN
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="43" role="option" class="ember-power-select-option">
+                          <span id="ember3581" class="ember-view">  TX
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="44" role="option" class="ember-power-select-option">
+                          <span id="ember3582" class="ember-view">  UT
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="45" role="option" class="ember-power-select-option">
+                          <span id="ember3583" class="ember-view">  VT
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="46" role="option" class="ember-power-select-option">
+                          <span id="ember3584" class="ember-view">  VA
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="47" role="option" class="ember-power-select-option">
+                          <span id="ember3585" class="ember-view">  WA
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="48" role="option" class="ember-power-select-option">
+                          <span id="ember3586" class="ember-view">  WV
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="49" role="option" class="ember-power-select-option">
+                          <span id="ember3587" class="ember-view">  WI
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="50" role="option" class="ember-power-select-option">
+                          <span id="ember3588" class="ember-view">  WY
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="51" role="option" class="ember-power-select-option">
+                          <span id="ember3589" class="ember-view">  GU
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="52" role="option" class="ember-power-select-option">
+                          <span id="ember3590" class="ember-view">  PR
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="53" role="option" class="ember-power-select-option">
+                          <span id="ember3591" class="ember-view">  VI
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="54" role="option" class="ember-power-select-option">
+                          <span id="ember3592" class="ember-view">  AE
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="55" role="option" class="ember-power-select-option">
+                          <span id="ember3593" class="ember-view">  AA
+</span>
+
+
+
+
+    </li>
+    <li aria-selected="false" aria-current="false" data-option-index="56" role="option" class="ember-power-select-option">
+                          <span id="ember3594" class="ember-view">  AP
+</span>
+
+
+
+
+    </li>
+</ul>
+"@ -split "`r`n" |
+Where-Object { $_ -match ' class="ember-view">'} |
+% {
+    $_ -split ">  " |
+    Select-Object -First 1 -Skip 1
+} |
+% {
+@"
+"$_",
+"@
+} ) -join ""
